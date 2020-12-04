f826f3a0a204b8f57b6a1e48e6119c093a20a1c9
Alejandro Rivero Perez <nubcrack@yahoo.es>
2020-12-04 07:07:28 +0100

    Final solution of AoC 2020 day 4.

--- 4/main.cpp
+++ 4/main.cpp
@@ -1,7 +1,5 @@
 #include <bits/stdc++.h>
 
-using namespace std;
-
 using ll = int64_t;
 using input_line_t = std::map<std::string, std::string>;
 using input_t = std::vector<input_line_t>;
@@ -46,7 +44,6 @@ input_t read_input() {
     std::string line;
     input_line_t p;
     while (std::getline(std::cin, line)) {
-        std::istringstream iss(line);
         if (line.empty()) {
             input_values.push_back(p);
             p.clear();
@@ -63,65 +60,50 @@ input_t read_input() {
 }
 
 ll solve1(input_t const& input_data) {
-    std::vector<std::string> req{ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
     ll ans = 0;
+    std::vector<std::string> required{ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
     for (auto const& p : input_data) {
-        bool bad = false;
-        for (auto const& r : req) {
-            auto const it = p.find(r);
-            if (it == p.cend()) {
-                bad = true;
-                break;
-            }
-        }
-        if (!bad)
+        auto const valid = std::all_of(std::cbegin(required), std::cend(required), [&](auto const& r) {
+            return p.find(r) != p.cend();
+        });
+        if (valid) {
             ans++;
+        }
     }
     return ans;
 }
 
-bool check_size(std::string& d) {
-    trim(d);
-    return (d.size() == 4);
-}
-
 bool byr_check(std::string const& d) {
-    auto dd = d;
-    if (!check_size(dd)) {
+    if (d.size() != 4) {
         return false;
     }
-    auto const n = std::stoi(dd);
+    auto const n = std::stoi(d);
     return 1920 <= n && n <= 2002;
 }
 bool iyr_check(std::string const& d) {
-    auto dd = d;
-    if (!check_size(dd)) {
+    if (d.size() != 4) {
         return false;
     }
-    auto const n = std::stoi(dd);
+    auto const n = std::stoi(d);
     return 2010 <= n && n <= 2020;
 }
 bool eyr_check(std::string const& d) {
-    auto dd = d;
-    if (!check_size(dd)) {
+    if (d.size() != 4) {
         return false;
     }
-    auto const n = std::stoi(dd);
+    auto const n = std::stoi(d);
     return 2020 <= n && n <= 2030;
 }
 bool hgt_check(std::string const& d) {
-    auto dd = d;
-    trim(dd);
-
-    if (dd.size() < 2) {
+    if (d.size() < 2) {
         return false;
     }
 
-    if (dd[dd.size() - 2] == 'c' && dd[dd.size() - 1] == 'm') {
-        auto v = std::stoi(dd.substr(0, dd.size() - 2));
+    if (d[d.size() - 2] == 'c' && d[d.size() - 1] == 'm') {
+        auto const v = std::stoi(d.substr(0, d.size() - 2));
         return 150 <= v && v <= 193;
-    } else if (dd[dd.size() - 2] == 'i' && dd[dd.size() - 1] == 'n') {
-        auto v = std::stoi(dd.substr(0, dd.size() - 2));
+    } else if (d[d.size() - 2] == 'i' && d[d.size() - 1] == 'n') {
+        auto const v = std::stoi(d.substr(0, d.size() - 2));
         return 59 <= v && v <= 76;
     }
 
@@ -131,10 +113,10 @@ bool hcl_check(std::string const& d) {
     if (d.size() != 7 || d[0] != '#') {
         return false;
     }
-    auto it = std::find_if(d.cbegin() + 1, d.cend(), [](auto const& c) {
+    auto const invalid_char = std::find_if(d.cbegin() + 1, d.cend(), [](auto const& c) {
         return !(('0' <= c && c <= '9') || ('a' <= c && c <= 'f'));
     });
-    return it == d.cend();
+    return invalid_char == d.cend();
 }
 bool ecl_check(std::string const& d) {
     static std::set<std::string> color{ "amb", "blu", "brn", "gry", "grn", "hzl", "oth" };
@@ -143,57 +125,35 @@ bool ecl_check(std::string const& d) {
 bool pid_check(std::string const& d) {
     if (d.size() != 9)
         return false;
-    return std::find_if(d.cbegin(),
-                        d.cend(),
-                        [](auto const& c) {
-                            return !('0' <= c && c <= '9');
-                        })
-           == d.cend();
+    auto const invalid_number = std::find_if(d.cbegin(), d.cend(), [](auto const& c) {
+        return !('0' <= c && c <= '9');
+    });
+    return invalid_number == d.cend();
 }
 
 ll solve2(input_t const& input_data) {
-    std::vector<std::string> req{ "byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid" };
-    std::vector<std::function<bool(std::string const&)>> req_f{ byr_check, iyr_check, eyr_check, hgt_check,
-                                                                hcl_check, ecl_check, pid_check };
-
+    using passport_element_t = std::pair<std::string, std::function<bool(std::string const&)>>;
+    std::vector<passport_element_t> const required{ { "byr", byr_check }, { "iyr", iyr_check }, { "eyr", eyr_check },
+                                                    { "hgt", hgt_check }, { "hcl", hcl_check }, { "ecl", ecl_check },
+                                                    { "pid", pid_check } };
     ll ans = 0;
     for (auto const& p : input_data) {
-        bool bad = false;
-        for (auto const& r : req) {
-            auto const it = p.find(r);
-            if (it == p.cend()) {
-                bad = true;
-                break;
+        auto const valid = std::all_of(std::cbegin(required), std::cend(required), [&](auto const& r) {
+            auto const e = p.find(r.first);
+            if (e == p.cend()) {
+                return false;
             }
-        }
-        if (!bad) {
-            bad = false;
-            for (int i = 0; i < req.size(); i++) {
-                auto e = p.find(req[i]);
-                if (!req_f[i](e->second)) {
-                    // std::cout << i << " " << e->second << " " << e->second.size() << std::endl;
-                    bad = true;
-                    break;
-                }
-            }
-            if (!bad) {
-                ans++;
-            }
+            return r.second(e->second);
+        });
+        if (valid) {
+            ans++;
         }
     }
     return ans;
 }
 
 int main() {
-    std::cout << hcl_check("#c0946f") << std::endl;
     auto const input_data = read_input();
-    std::for_each(std::cbegin(input_data), std::cbegin(input_data) + 1, [](auto const& elem) {
-        std::for_each(std::cbegin(elem), std::cend(elem), [](auto const& e) {
-            std::cout << e.first << ": " << e.second << ", ";
-        });
-        std::cout << std::endl;
-    });
-    std::cout << std::endl;
 
     auto const s1 = solve1(input_data);
     std::cout << "Solution Part1: " << s1 << std::endl;
