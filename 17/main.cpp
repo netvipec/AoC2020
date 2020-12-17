7ee987be727a332e16fbf521137ac71510c51cec
Alejandro Rivero Perez <nubcrack@yahoo.es>
2020-12-17 11:28:12 +0100

    Added another solution to day 17 of AoC 2020.

--- 17/main.cpp
+++ 17/main.cpp
@@ -7,27 +7,57 @@ struct point {
     ll y = 0;
     ll z = 0;
     ll w = 0;
+
+    inline bool operator==(const point& e) const { return x == e.x && y == e.y && z == e.z && w == e.w; }
 };
 
-using row_t = std::vector<bool>;
-using input_t = std::vector<row_t>;
+inline void hash_combine(std::size_t& seed) {}
+
+template <typename T, typename... Rest>
+inline void hash_combine(std::size_t& seed, const T& v, Rest... rest) {
+    std::hash<T> hasher;
+    seed ^= hasher(v) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    hash_combine(seed, rest...);
+}
+
+namespace std {
+    template <>
+    struct hash<point> {
+        using argument_type = point;
+        using result_type = std::size_t;
+        result_type operator()(argument_type const& a) const {
+            result_type const h1(std::hash<int>()(a.x));
+            result_type const h2(std::hash<int>()(a.y));
+            result_type const h3(std::hash<int>()(a.z));
+            result_type const h4(std::hash<int>()(a.w));
+            result_type seed = 0;
+            hash_combine(seed, h1, h2, h3, h4);
+            return seed;
+        }
+    };
+} // namespace std
+
+using input_t = std::unordered_set<point>;
 using result_t = ll;
 
 input_t read_input() {
     input_t input_values;
 
+    ll idx = 0;
     std::string line;
     while (std::getline(std::cin, line)) {
         if (line.empty()) {
             break;
         }
 
-        std::vector<bool> row;
-        std::transform(std::cbegin(line), std::cend(line), std::back_inserter(row), [](auto const& c) {
-            return c == '#';
+        ll inner_idx = 0;
+        std::for_each(std::cbegin(line), std::cend(line), [&](auto const& c) {
+            if (c == '#') {
+                input_values.insert(point{ idx, inner_idx });
+            }
+            inner_idx++;
         });
-
-        input_values.emplace_back(row);
+        idx++;
     }
     return input_values;
 }
@@ -59,43 +89,40 @@ std::vector<point> get_neighbors(bool three_dimention = true) {
 auto const neighbors_3d = get_neighbors();
 auto const neighbors_4d = get_neighbors(false);
 
-inline bool is_valid_3d(point const& p, ll rows, ll columns) {
-    return (1 <= p.x && p.x <= rows) && (1 <= p.y && p.y <= columns) && (1 <= p.z && p.z <= columns);
-}
+void evolve1(input_t& cube) {
+    input_t new_cube;
 
-inline bool get_cell(std::vector<input_t> const& cube, point const& p, ll displacement = 0) {
-    return cube[p.x - displacement][p.y - displacement][p.z - displacement];
-}
+    auto const mm_x = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.x < rhs.x;
+    });
+    auto const mm_y = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.y < rhs.y;
+    });
+    auto const mm_z = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.z < rhs.z;
+    });
 
-void evolve(std::vector<input_t>& cube) {
-    auto const cube_rows = cube.size();
-    auto const cube_columns = cube.front().size();
-    std::vector<input_t> new_cube(cube_rows + 2, input_t(cube_columns + 2, std::vector<bool>(cube_columns + 2, false)));
-
-    for (ll i = 0; i < (ll)new_cube.size(); i++) {
-        for (ll j = 0; j < (ll)new_cube[i].size(); j++) {
-            for (ll k = 0; k < (ll)new_cube[i][j].size(); k++) {
+    for (ll i = mm_x.first->x - 1; i <= mm_x.second->x + 1; i++) {
+        for (ll j = mm_y.first->y - 1; j <= mm_y.second->y + 1; j++) {
+            for (ll k = mm_z.first->z - 1; k <= mm_z.second->z + 1; k++) {
                 ll active = 0;
                 for (auto const& n : neighbors_3d) {
                     point np;
                     np.x = i + n.x;
                     np.y = j + n.y;
                     np.z = k + n.z;
-                    if (is_valid_3d(np, cube_rows, cube_columns)) {
-                        active += (get_cell(cube, np, 1) ? 1 : 0);
+                    if (cube.find(np) != std::cend(cube)) {
+                        active++;
                     }
                 }
 
                 point const pp{ i, j, k };
-                if (is_valid_3d(pp, cube_rows, cube_columns)) {
-                    auto const cell_value = get_cell(cube, pp, 1);
-                    if (cell_value && (active == 2 || active == 3)) {
-                        new_cube[i][j][k] = true;
-                    } else if (!cell_value && active == 3) {
-                        new_cube[i][j][k] = true;
+                if (cube.find(pp) != std::cend(cube)) {
+                    if (active == 2 || active == 3) {
+                        new_cube.insert(pp);
                     }
                 } else if (active == 3) {
-                    new_cube[i][j][k] = true;
+                    new_cube.insert(pp);
                 }
             }
         }
@@ -104,55 +131,26 @@ void evolve(std::vector<input_t>& cube) {
     std::swap(cube, new_cube);
 }
 
-void print_cube_3d(std::vector<input_t> const& cube) {
-    std::for_each(std::cbegin(cube), std::cend(cube), [](auto const& l) {
-        std::for_each(std::cbegin(l), std::cend(l), [](auto const& r) {
-            std::for_each(std::cbegin(r), std::cend(r), [](auto const& c) {
-                std::cout << (c ? '#' : '.');
-            });
-            std::cout << std::endl;
-        });
-        std::cout << std::endl << std::endl;
-    });
-    std::cout << std::endl;
-}
+void evolve2(input_t& cube) {
+    input_t new_cube;
 
-result_t solve1(input_t const& input_data) {
-    std::vector<input_t> cube{ input_data };
-    for (int cycle = 1; cycle <= 6; cycle++) {
-        evolve(cube);
-        // if (cycle < 3)
-        //     print_cube_3d(cube);
-    }
-    result_t ans = std::accumulate(std::cbegin(cube), std::cend(cube), 0ll, [](auto const& b1, auto const& l) {
-        return b1 + std::accumulate(std::cbegin(l), std::cend(l), 0ll, [](auto const& b2, auto const& r) {
-                   return b2 + std::count(std::cbegin(r), std::cend(r), true);
-               });
+    auto const mm_x = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.x < rhs.x;
+    });
+    auto const mm_y = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.y < rhs.y;
+    });
+    auto const mm_z = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.z < rhs.z;
+    });
+    auto const mm_w = std::minmax_element(std::cbegin(cube), std::cend(cube), [](auto const& lhs, auto const& rhs) {
+        return lhs.w < rhs.w;
     });
 
-    return ans;
-}
-
-inline bool is_valid_4d(point const& p, ll rows, ll columns) {
-    return (1 <= p.x && p.x <= rows) && (1 <= p.y && p.y <= rows) && (1 <= p.z && p.z <= columns)
-           && (1 <= p.w && p.w <= columns);
-}
-
-inline bool get_cell(std::vector<std::vector<input_t>> const& cube, point const& p, ll displacement = 0) {
-    return cube[p.x - displacement][p.y - displacement][p.z - displacement][p.w - displacement];
-}
-
-void evolve2(std::vector<std::vector<input_t>>& cube) {
-    auto const cube_rows = cube.size();
-    auto const cube_columns = cube.front().front().size();
-    std::vector<std::vector<input_t>> new_cube(
-        cube_rows + 2,
-        std::vector<input_t>(cube_rows + 2, input_t(cube_columns + 2, std::vector<bool>(cube_columns + 2, false))));
-
-    for (ll i = 0; i < (ll)new_cube.size(); i++) {
-        for (ll j = 0; j < (ll)new_cube[i].size(); j++) {
-            for (ll k = 0; k < (ll)new_cube[i][j].size(); k++) {
-                for (ll l = 0; l < (ll)new_cube[i][j].size(); l++) {
+    for (ll i = mm_x.first->x - 1; i <= mm_x.second->x + 1; i++) {
+        for (ll j = mm_y.first->y - 1; j <= mm_y.second->y + 1; j++) {
+            for (ll k = mm_z.first->z - 1; k <= mm_z.second->z + 1; k++) {
+                for (ll l = mm_w.first->w - 1; l <= mm_w.second->w + 1; l++) {
                     ll active = 0;
                     for (auto const& n : neighbors_4d) {
                         point np;
@@ -160,21 +158,18 @@ void evolve2(std::vector<std::vector<input_t>>& cube) {
                         np.y = j + n.y;
                         np.z = k + n.z;
                         np.w = l + n.w;
-                        if (is_valid_4d(np, cube_rows, cube_columns)) {
-                            active += (get_cell(cube, np, 1) ? 1 : 0);
+                        if (cube.find(np) != std::cend(cube)) {
+                            active++;
                         }
                     }
 
                     point const pp{ i, j, k, l };
-                    if (is_valid_4d(pp, cube_rows, cube_columns)) {
-                        auto const cell_value = get_cell(cube, pp, 1);
-                        if (cell_value && (active == 2 || active == 3)) {
-                            new_cube[i][j][k][l] = true;
-                        } else if (!cell_value && active == 3) {
-                            new_cube[i][j][k][l] = true;
+                    if (cube.find(pp) != std::cend(cube)) {
+                        if (active == 2 || active == 3) {
+                            new_cube.insert(pp);
                         }
                     } else if (active == 3) {
-                        new_cube[i][j][k][l] = true;
+                        new_cube.insert(pp);
                     }
                 }
             }
@@ -184,51 +179,26 @@ void evolve2(std::vector<std::vector<input_t>>& cube) {
     std::swap(cube, new_cube);
 }
 
-void print_cube_4d(std::vector<std::vector<input_t>> const& cube) {
-    std::for_each(std::cbegin(cube), std::cend(cube), [](auto const& d) {
-        std::for_each(std::cbegin(d), std::cend(d), [](auto const& l) {
-            std::for_each(std::cbegin(l), std::cend(l), [](auto const& r) {
-                std::for_each(std::cbegin(r), std::cend(r), [](auto const& c) {
-                    std::cout << (c ? '#' : '.');
-                });
-                std::cout << std::endl;
-            });
-            std::cout << std::endl << std::endl;
-        });
-        std::cout << std::endl << std::endl;
-    });
-    std::cout << std::endl;
+result_t solve1(input_t const& input_data) {
+    input_t cube = input_data;
+    for (int cycle = 1; cycle <= 6; cycle++) {
+        evolve1(cube);
+    }
+    return cube.size();
 }
 
 result_t solve2(input_t const& input_data) {
-    std::vector<std::vector<input_t>> cube{ { input_data } };
+    input_t cube = input_data;
     for (int cycle = 1; cycle <= 6; cycle++) {
         evolve2(cube);
-        // if (cycle < 3)
-        //     print_cube_4d(cube);
     }
-    result_t ans = std::accumulate(std::cbegin(cube), std::cend(cube), 0ll, [](auto const& b1, auto const& c) {
-        return b1 + std::accumulate(std::cbegin(c), std::cend(c), 0ll, [](auto const& b2, auto const& l) {
-                   return b2 + std::accumulate(std::cbegin(l), std::cend(l), 0ll, [](auto const& b3, auto const& r) {
-                              return b3 + std::count(std::cbegin(r), std::cend(r), true);
-                          });
-               });
-    });
-
-    return ans;
+    return cube.size();
 }
 
 int main() {
     auto begin_input = std::chrono::steady_clock::now();
     auto const input_data = read_input();
     auto end_input = std::chrono::steady_clock::now();
-    // std::for_each(std::cbegin(input_data), std::cend(input_data), [](auto const& r) {
-    //     std::for_each(std::cbegin(r), std::cend(r), [](auto const& c) {
-    //         std::cout << c << ", ";
-    //     });
-    //     std::cout << std::endl;
-    // });
-    // std::cout << std::endl;
     std::cout << "Read Input Time = "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end_input - begin_input).count() << "[ms]"
               << std::endl;
